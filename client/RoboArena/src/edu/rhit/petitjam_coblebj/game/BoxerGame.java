ad641dc636d9e4eef655ed8c82ddc699561fad71
Alex Petitjean <petitjam@rose-hulman.edu>
2013-10-26 16:09:40 -0400

    added gesture listener to ArenaActivity

--- client/RoboArena/src/edu/rhit/petitjam_coblebj/game/BoxerGame.java
+++ client/RoboArena/src/edu/rhit/petitjam_coblebj/game/BoxerGame.java
@@ -1,6 +1,10 @@
 package edu.rhit.petitjam_coblebj.game;
 
+import android.util.Log;
+
 public class BoxerGame {
+	
+	private static final String BG = "BG";
 	
 	private LocalPlayer local;
 	private RemotePlayer remote;
@@ -35,59 +39,60 @@ public class BoxerGame {
 			// check what happens to other player (check if block, etc) (also, maybe if both Ljab at the same time, they hit midair?)
 			// more update game view? idk
 		// }
+		Log.d(BG, "Local Left Jab");
 	}
 	
 	public void localRightJab() {
-		
+		Log.d(BG, "Local Right Jab");
 	}
 	
 	public void localLeftHook() {
-		
+		Log.d(BG, "Local Left Hook");
 	}
 	
 	public void localRightHook() {
-		
+		Log.d(BG, "Local Right Hook");
 	}
 	
 	public void localLeftUppercut() {
-		
+		Log.d(BG, "Local Left Uppercut");
 	}
 	
 	public void localRightUppercut() {
-		
+		Log.d(BG, "Local Right Uppercut");
 	}
 	
 	public void localBlock() {
-		
+		Log.d(BG, "Local Block");
 	}
 	
 	/* REMOTE PLAYER ACTIONS */
 	
 	public void remoteLeftJab() {
-		
+		Log.d(BG, "Remote Left Jab");
 	}
 	
 	public void remoteRightJab() {
-		
+		Log.d(BG, "Remote Right Jab");
 	}
 	
 	public void remoteLeftHook() {
-		
+		Log.d(BG, "Remote Left Hook");
 	}
 	
 	public void remoteRightHook() {
-		
+		Log.d(BG, "Remote Right Hook");
 	}
 	
 	public void remoteLeftUppercut() {
-		
+		Log.d(BG, "Remote Left Uppercut");
 	}
 	
 	public void remoteRightUppercut() {
-		
+		Log.d(BG, "Remote Righ Uppercut");
 	}
 	
 	public void remoteBlock() {
-		
+		Log.d(BG, "Remote Block");
 	}
 }
--- client/RoboArena/src/edu/rhit/petitjam_coblebj/roboarena/ArenaActivity.java
+++ client/RoboArena/src/edu/rhit/petitjam_coblebj/roboarena/ArenaActivity.java
@@ -2,14 +2,113 @@ package edu.rhit.petitjam_coblebj.roboarena;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import edu.rhit.petitjam_coblebj.game.BoxerGame;
 
 public class ArenaActivity extends Activity{
 
 	// Fields
-	
+	private GestureDetector mDetector;
+	private BoxerGame mGame;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.arena);
+		
+		mGame = new BoxerGame();
+		
+		mDetector = new GestureDetector(this, new PlayerGestureDetector());
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getPointerCount() > 1) {
+			Log.d("PGL", "two finger event");
+		}
+		return mDetector.onTouchEvent(event);
+	}
+	
+	private class PlayerGestureDetector extends GestureDetector.SimpleOnGestureListener {
+		private static final String PGL = "PGL";
+
+		/* Constants for gesture regions */
+		private final int CENTER_X = ArenaActivity.this.getResources().getDisplayMetrics().widthPixels / 2;
+		
+		/* Constants for gesture swipe restrictions */
+		private static final int SWIPE_MIN_VELOCITY = 100;
+		private static final int SWIPE_MAX_OFF_PATH = 100;
+		private static final int SWIPE_MIN_DISTANCE = 100;
+		
+		/*
+		 * Handles Jabs 
+		 */
+		@Override
+		public boolean onSingleTapUp(MotionEvent e) {
+			float x = e.getX();
+			float y = e.getY();
+			
+			if (x >= CENTER_X) {
+				Log.d(PGL, "right jab");
+				mGame.localRightJab();
+			} else {
+				Log.d(PGL, "left jab");
+				mGame.localLeftJab();
+			}
+			
+			return true;
+		}
+		
+		/*
+		 * Handles uppercuts, hooks, and blocks
+		 */
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			
+			float e1_x = e1.getX();
+			float e2_x = e2.getX();
+			float e1_y = e1.getY();
+			float e2_y = e2.getY();
+			
+			float dx = e2_x - e1_x;
+			float dy = e2_y - e1_y;
+			
+			if (Math.abs(velocityX) >= SWIPE_MIN_VELOCITY || Math.abs(velocityY) >= SWIPE_MIN_VELOCITY) { // Swipe fast enough
+				
+				if (e1_x >= CENTER_X) { // Right hits
+					
+					if (dx < SWIPE_MAX_OFF_PATH && dy < 0) { // swipe up
+						Log.d(PGL, "right uppercut");
+					} 
+					else if (dy < SWIPE_MAX_OFF_PATH && dx < 0) { // swipe r->l
+						Log.d(PGL, "right hook");
+					} 
+					else {
+						Log.d(PGL, "right nothing");
+					}
+					
+				} else { 				// Left hits
+					
+					if (dx < SWIPE_MAX_OFF_PATH && dy < 0) { // swipe up
+						Log.d(PGL, "left uppercut");
+					} 
+					else if (dy < SWIPE_MAX_OFF_PATH && dx > 0) { // swipe r->l
+						Log.d(PGL, "left hook");
+					} 
+					else {
+						Log.d(PGL, "left nothing");
+					}
+					
+				}
+			}
+			else {
+				Log.d(PGL, "not fast enough");
+			}
+			
+			
+			return true;
+		}
 	}
 }
